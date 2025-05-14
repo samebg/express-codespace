fa84dc66586e72cbc791c56cdb3eaf06d2bfe2f5
samebg <samebengho@gmail.com>
2025-05-14 17:55:34 +0000

    updated

--- server.js
+++ server.js
@@ -8,9 +8,25 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
 
+  switch(req.url){
+    case '/':
+      res.statusCode = 200;
+      res.end('<h1>Welcome to Node.js!</h1><p>Please add interesting info here.</p><')
+    break;
+    
+    case '/about':
+       res.statusCode = 200;
+       res.end('<h1>Welcome to Sam Ebengho\'s Page</h1><p>Please add interesting info here.</p>');
+    break;
+    
+    default:
+       res.statusCode = 404;
+       res.end('<h1>File not found</h1><p>Please add interesting info here.</p>');
+  }
+
+
   //default HTML
-  res.statusCode = 200;
-  res.end('<h1>Welcome to Node.js!</h1><p>Please add interesting info here.</p>');
+
 
 });
 
